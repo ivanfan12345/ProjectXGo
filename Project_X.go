fd5488c342fe28ff4afdd349fd6a9f68493533b9
Ivan Fan <icfan1989@gmail.com>
2015-07-16 20:59:49 -0600

    removed unnecessary  files and added function to arrays_and_strings.go

--- Project_X.go
+++ Project_X.go
@@ -3,7 +3,7 @@
 package main
 import "fmt"
 import "strings"
-import "sort"
+
 
 func main() {
 	fmt.Println(" ========== Welcome to ProjectX Go ==========\n\n")
@@ -23,35 +23,3 @@ func Detect_Hacker_Name() {
 	fmt.Scanf("%s",&input)
 	fmt.Println("Welcome ", input ,"let's hack!")
 }
-
-//Write a function to detect if two strings are anagrams of each other.
-func Detect_if_anagram(str1 string , str2 string) bool {
-
-	var length1 = len(str1)
-	var length2 = len(str2)
-
-	if length1 != length2 {
-				return false
-	} else {
-				str1 := Sort_String(str1)
-				str2 := Sort_String(str2)
-
-				fmt.Println("1. " , str1)
-				fmt.Println("2. " , str2)
-
-				if str1 == str2 {
-					return true
-				} else {
-					return false
-				}
-
-			  return true
-	}
-}
-
-//Function to sort strings using slices?
-func Sort_String(str string) string {
-		newStr := strings.Split(str, "")
-		sort.Strings(newStr)
-		return strings.Join(newStr,"")
-	}
--- arrays_and_strings.go
+++ arrays_and_strings.go
@@ -2,7 +2,37 @@ package arrays_and_strings
 
 //import "strings"
 import "fmt"
+import "sort"
+import "strings"
 
-func Detect_if_anagram(string) {
-    fmt.Println("Hello")
+//Write a function to detect if two strings are anagrams of each other.
+func Detect_if_anagram(str1 string , str2 string) bool {
+
+	var length1 = len(str1)
+	var length2 = len(str2)
+
+	if length1 != length2 {
+				return false
+	} else {
+				str1 := Sort_String(str1)
+				str2 := Sort_String(str2)
+
+				fmt.Println("1. " , str1)
+				fmt.Println("2. " , str2)
+
+				if str1 == str2 {
+					return true
+				} else {
+					return false
+				}
+
+			  return true
+	}
 }
+
+//Function to sort strings using slices?
+func Sort_String(str string) string {
+		newStr := strings.Split(str, "")
+		sort.Strings(newStr)
+		return strings.Join(newStr,"")
+	}
